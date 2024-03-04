7c2896b4fe6c3cff98a28861250e1caa5ec164b5
Hangjen Chan <marxlenin2001@gmail.com>
2024-03-04 08:05:42 +0100

    final version update

--- Final_Version/main.py
+++ Final_Version/main.py
@@ -1,22 +1,26 @@
 from objective import objective
 from benchmark import benchmark
 from path_protection import path_protection
+from shared_protection import shared_protection
 import matplotlib.pyplot as plt
 import numpy as np
 # import pandas as pd
 
 def main():
-    topology_file = 'G7-topology.txt'
-    matrix_file = 'G7-matrix-5.txt'
+    topology_file = 'IT10-topology.txt'
+    matrix_file = 'IT10-matrix-5.txt'
     num_fsu_used,highest_load,highest_fsu,average_fsu_used,average_path_length,total_cost=objective(topology_file, matrix_file)
     num_fsu_used_benchmark,highest_load_benchmark,highest_fsu_benchmark,average_fsu_used_benchmark,average_path_length_benchmark,total_cost_benchmark=benchmark(topology_file, matrix_file)
     num_fsu_used_path_protection,highest_load_path_protection,highest_fsu_path_protection,average_fsu_used_path_protection,average_path_length_path_protection,total_cost_path_protection=path_protection(topology_file, matrix_file)
+    num_fsu_used_shared_protection,highest_load_shared_protection,highest_fsu_shared_protection,average_fsu_used_shared_protection,average_path_length_shared_protection,total_cost_shared_protection=shared_protection(topology_file, matrix_file)
     print(num_fsu_used,highest_load,highest_fsu,average_fsu_used,average_path_length,total_cost)
     print(num_fsu_used_benchmark,highest_load_benchmark,highest_fsu_benchmark,average_fsu_used_benchmark,average_path_length_benchmark,total_cost_benchmark)
     print(num_fsu_used_path_protection,highest_load_path_protection,highest_fsu_path_protection,average_fsu_used_path_protection,average_path_length_path_protection,total_cost_path_protection)
+    print(num_fsu_used_shared_protection,highest_load_shared_protection,highest_fsu_shared_protection,average_fsu_used_shared_protection,average_path_length_shared_protection,total_cost_shared_protection)
     data = [num_fsu_used,highest_load,highest_fsu,average_fsu_used,average_path_length,total_cost]
     data_benchmark = [num_fsu_used_benchmark,highest_load_benchmark,highest_fsu_benchmark,average_fsu_used_benchmark,average_path_length_benchmark,total_cost_benchmark]
     data_path_protection = [num_fsu_used_path_protection,highest_load_path_protection,highest_fsu_path_protection,average_fsu_used_path_protection,average_path_length_path_protection,total_cost_path_protection]
+    data_shared_protection = [num_fsu_used_shared_protection,highest_load_shared_protection,highest_fsu_shared_protection,average_fsu_used_shared_protection,average_path_length_shared_protection,total_cost_shared_protection]
 
     labels = ['FSU Used', 'Highest Load', 'Highest FSU', 'Average FSU Used', 'Average Path Length', 'Total Cost']
 
@@ -24,10 +28,11 @@ def main():
     matrix_file = matrix_file.split('.')[0]
 
     x = np.arange(1)
-    width = 0.35
-    ax1.bar(x - width , [highest_load], width, label='Our method')
-    ax1.bar(x, [highest_load_benchmark], width, label='Benchmark')
-    ax1.bar(x + width , [highest_load_path_protection], width, label='Path Protection')
+    width = 0.25
+    ax1.bar(x - 1.5*width , [highest_load], width, label='Our method')
+    ax1.bar(x-0.5*width, [highest_load_benchmark], width, label='Benchmark')
+    ax1.bar(x + 0.5*width , [highest_load_path_protection], width, label='Path Protection')
+    ax1.bar(x + 1.5*width, [highest_load_shared_protection], width, label='Shared Protection')
     ax1.set_ylabel('Value')
     ax1.set_title('Maximum Link Load Comparison')
     ax1.set_xticks(x)
@@ -35,10 +40,11 @@ def main():
     ax1.legend()
 
     x = np.arange(len(data) - 1)
-    width = 0.3
-    ax2.bar(x - width , data[:1] + data[2:], width, label='Our method')
-    ax2.bar(x , data_benchmark[:1] + data_benchmark[2:], width, label='Benchmark')
-    ax2.bar(x + width, data_path_protection[:1] + data_path_protection[2:], width, label='Path Protection')
+    width = 0.25
+    ax2.bar(x - 1.5*width , data[:1] + data[2:], width, label='Our method')
+    ax2.bar(x -0.5*width, data_benchmark[:1] + data_benchmark[2:], width, label='Benchmark')
+    ax2.bar(x + 0.5*width, data_path_protection[:1] + data_path_protection[2:], width, label='Path Protection')
+    ax2.bar(x + 1.5*width, data_shared_protection[:1] + data_shared_protection[2:], width, label='Shared Protection')
     ax2.set_ylabel('Value')
     ax2.set_title('Other Comparisons')
     ax2.set_xticks(x)
---
+++ Final_Version/shared_protection.py
@@ -0,0 +1,372 @@
+import copy
+import random
+import math
+import pickle
+import networkx as nx
+import matplotlib.pyplot as plt
+from itertools import product
+from matplotlib.patches import Rectangle
+
+
+
+def preprocessing_topogy(file_path):
+    edges = []
+    with open(file_path, 'r') as file:
+        next(file)  # Skip header
+        for line in file:
+            parts = line.strip().split('\t')
+            link_id = int(parts[0])
+            node_a = int(parts[3])
+            node_b = int(parts[4])
+            length = int(parts[5])
+            edges.append((node_a, node_b, length))
+            edges.append((node_b, node_a, length))  # Adding reverse edge
+    return edges
+
+def preprocessing_traffic(file_path):
+    traffic_matrix = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            parts = line.strip().split('\t')
+            row_data = [int(x) for x in parts]
+            traffic_matrix.append(row_data)
+    return traffic_matrix
+
+def shared_protection(topology_file, traffic_file):
+    traffic_file_name = traffic_file.replace('.txt', '')
+    edges= preprocessing_topogy(topology_file)
+    traffic_matrix= preprocessing_traffic(traffic_file)
+
+    G = nx.DiGraph()
+    G.add_weighted_edges_from(edges)
+
+    ksp = {}
+    for i, j in product(range(7), repeat=2):
+        if i != j:
+            source, target = i + 1, j + 1
+            ksp[(source, target)] = list(nx.shortest_simple_paths(G, source, target, weight='weight'))[:5]
+
+    pos = nx.spring_layout(G)
+    plt.figure(figsize=(12, 8))
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray')
+    edge_labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    plt.title("Initial Network Topology")
+    plt.show()
+
+    initial_paths = {}
+    inintial_back_up_paths= {}
+    for (source, target), paths in ksp.items():
+        initial_paths[(source, target)] = paths[0]
+        inintial_back_up_paths[(source, target)] = paths[1]
+
+    edge_flows = {(u, v): 0 for u, v in G.edges()}
+    for (source, target), path in initial_paths.items():
+        flow = traffic_matrix[source - 1][target - 1]
+        for i in range(len(path) - 1):
+            edge_flows[(path[i], path[i + 1])] += flow
+
+    for (source, target), path in inintial_back_up_paths.items():
+        flow = traffic_matrix[source - 1][target - 1]
+        for i in range(len(path) - 1):
+            edge_flows[(path[i], path[i + 1])] += flow
+
+    # 固定随机种子:2001110011 for IT-Matrix-5:1027
+    # 固定随机种子:1989604 for IT-Matrix-4:738
+    # 固定随机种子:110101195306153019 for IT-Matrix-3:445 #感谢提供身份证号码的神必人士
+    # 固定随机种子:114514 for IT-Matrix-3:238
+    # 固定随机种子:19890604 for IT-Matrix-2:84
+
+    random.seed(19890604)
+
+    def calculate_max_edge_load(edge_flows):
+        return max(edge_flows.values())
+
+    def simulated_annealing(G, ksp, traffic_matrix, initial_temp, final_temp, alpha, max_iter):
+        current_temp = initial_temp
+        current_state = initial_paths.copy()
+        current_edge_flows = edge_flows.copy()
+        current_max_load = calculate_max_edge_load(current_edge_flows)
+        current_state_backup = inintial_back_up_paths.copy()
+
+
+        best_state = current_state
+        best_edge_flows = current_edge_flows
+        best_max_load = current_max_load
+        best_state_backup = current_state_backup
+
+        for i in range(max_iter):
+            # 降温
+            current_temp *= alpha
+            if current_temp < final_temp:
+                break
+
+            request = random.choice(list(ksp.keys()))
+            new_path = random.choice(ksp[request][1:])
+            new_back_up_path = random.choice(ksp[request][:1]+ksp[request][2:])
+            if new_path==new_back_up_path:
+                new_back_up_path = random.choice(ksp[request][:1]+ksp[request][2:])
+
+            new_edge_flows = current_edge_flows.copy()
+            flow = traffic_matrix[request[0] - 1][request[1] - 1]
+            old_path = current_state[request]
+            old_back_up_path = current_state_backup[request]
+
+            for i in range(len(old_path) - 1):
+                new_edge_flows[(old_path[i], old_path[i + 1])] -= flow
+            for i in range(len(old_back_up_path) - 1):
+                new_edge_flows[(old_back_up_path[i], old_back_up_path[i + 1])] -= flow
+
+            for i in range(len(new_path) - 1):
+                new_edge_flows[(new_path[i], new_path[i + 1])] += flow
+            for i in range(len(new_back_up_path) - 1):
+                new_edge_flows[(new_back_up_path[i], new_back_up_path[i + 1])] += flow
+
+            new_max_load = calculate_max_edge_load(new_edge_flows)
+
+            # if new_max_load < current_max_load or random.random() < math.exp((current_max_load - new_max_load) / current_temp):
+            if new_max_load < current_max_load:
+                current_state = current_state.copy()
+                current_state_backup = current_state_backup.copy()
+                current_state[request] = new_path
+                current_state_backup[request] = new_back_up_path
+
+                current_edge_flows = new_edge_flows
+                current_max_load = new_max_load
+
+                if new_max_load < best_max_load:
+                    best_state = current_state.copy()
+                    best_state_backup = current_state_backup.copy()
+                    best_edge_flows = current_edge_flows.copy()
+                    best_max_load = new_max_load
+
+        return best_state, best_edge_flows, best_max_load, best_state_backup
+
+    # 退火参数
+    initial_temp = 1000
+    final_temp = 1
+    alpha = 0.999999
+    max_iter = 100000
+
+    best_state, best_edge_flows, best_max_load, best_state_backup = simulated_annealing(G, ksp, traffic_matrix, initial_temp, final_temp,
+                                                                     alpha, max_iter)
+    #print(f"Best max edge load: {best_max_load}")
+
+    def draw_network(G, pos, edge_flows, title):
+        plt.figure(figsize=(12, 8))
+        nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray', node_size=500)
+
+        edge_labels = {(u, v): f'{d["weight"]}\n{edge_flows.get((u, v), 0)}' for u, v, d in G.edges(data=True)}
+
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, label_pos=0.3)
+        plt.title(title)
+        plt.savefig('objective_data_fig/optimized_network_topology-shared-'+traffic_file_name+'.png')
+        plt.show()
+
+    draw_network(G, pos, best_edge_flows, "Optimized Network Topology")
+
+    edge_traffic_details = {}
+
+    for request, path in best_state.items():
+        source, target = request
+        flow = traffic_matrix[source - 1][target - 1]
+        for i in range(len(path) - 1):
+            edge = (path[i], path[i + 1])
+            if edge not in edge_traffic_details:
+                edge_traffic_details[edge] = []
+            edge_traffic_details[edge].append({'path': path, 'flow': flow, 'source_target': (source, target)})
+
+    for request, path in best_state_backup.items():
+        source, target = request
+        flow = traffic_matrix[source - 1][target - 1]
+        for i in range(len(path) - 1):
+            edge = (path[i], path[i + 1])
+            if edge not in edge_traffic_details:
+                edge_traffic_details[edge] = []
+            edge_traffic_details[edge].append({'path': path, 'flow': flow, 'source_target': (source, target)})
+
+    # for edge, details in edge_traffic_details.items():
+         #print(f"Edge {edge}:")
+    #     for detail in details:
+            #print(f"  Path: {detail['path']}, Flow: {detail['flow']}, Request: {detail['source_target']}")
+
+    a = 1 + 1
+
+    FSU_CAPACITY = 320
+    CABLE_CAPACITY = 20
+    FSU_PER_CABLE = 3
+    COST_PER_CABLE = 2
+
+    spectrum_usage = {edge: [0] * FSU_CAPACITY for edge in G.edges()}
+
+    spectrum_allocation = {}
+    spectrum_allocation_backup = {}
+
+    def allocate_spectrum(request, path, flow, spectrum_usage):
+        fsu_needed = int(math.ceil(flow / CABLE_CAPACITY)) * FSU_PER_CABLE
+        fsu_availability = [float('inf')] * (FSU_CAPACITY - fsu_needed + 1)
+
+        for start_index in range(FSU_CAPACITY - fsu_needed + 1):
+            fsu_sum = 0
+            for i in range(len(path) - 1):
+                edge = (path[i], path[i + 1])
+                fsu_sum += sum(spectrum_usage[edge][start_index:start_index + fsu_needed])
+            fsu_availability[start_index] = fsu_sum
+
+        min_usage = min(fsu_availability)
+        fsu_index = fsu_availability.index(min_usage)
+
+        for i in range(len(path) - 1):
+            edge = (path[i], path[i + 1])
+            for j in range(fsu_index, fsu_index + fsu_needed):
+                spectrum_usage[edge][j] += 1
+
+        return fsu_index, fsu_needed
+
+    def allocate_spectrum_shared(request, path, flow, spectrum_usage,spectrum_usage_shared):
+        fsu_needed = int(math.ceil(flow / CABLE_CAPACITY)) * FSU_PER_CABLE
+        fsu_availability = [float('inf')] * (FSU_CAPACITY - fsu_needed + 1)
+
+        for start_index in range(FSU_CAPACITY - fsu_needed + 1):
+            fsu_sum = 0
+            for i in range(len(path) - 1):
+                edge = (path[i], path[i + 1])
+                fsu_sum += sum(spectrum_usage[edge][start_index:start_index + fsu_needed])
+            fsu_availability[start_index] = fsu_sum
+
+        min_usage = min(fsu_availability)
+        fsu_index = fsu_availability.index(min_usage)
+
+        for i in range(len(path) - 1):
+            edge = (path[i], path[i + 1])
+            for j in range(fsu_index, fsu_index + fsu_needed):
+                spectrum_usage_shared[edge][j] += 1
+
+        return fsu_index, fsu_needed, spectrum_usage_shared
+
+    for request, path in best_state.items():
+        source, target = request
+        flow = traffic_matrix[source - 1][target - 1]
+        fsu_index, fsu_needed = allocate_spectrum(request, path, flow, spectrum_usage)
+        spectrum_allocation[request] = {'fsu_index': fsu_index, 'fsu_needed': fsu_needed,'path': path}
+    spectrum_usage_shared = copy.deepcopy(spectrum_usage)
+    for request_backup, path_backup in best_state_backup.items():
+        source, target = request
+        flow = traffic_matrix[source - 1][target - 1]
+        fsu_index_backup, fsu_needed_backup, spectrum_usage_shared = allocate_spectrum_shared(request_backup, path_backup, flow, spectrum_usage,spectrum_usage_shared)
+        spectrum_allocation_backup[request_backup] = {'fsu_index': fsu_index_backup, 'fsu_needed': fsu_needed_backup,'path': path_backup}
+
+
+
+
+
+
+
+
+
+    def draw_spectrum_allocation(best_state, best_state_backup, spectrum_allocation, spectrum_allocation_backup):
+        fig, ax = plt.subplots(figsize=(20, 20))
+        color_map = {}
+        for i, request in enumerate(spectrum_allocation.keys()):
+            color_map[request] = plt.cm.tab20(i % 20)
+
+        edge_to_row = {edge: i for i, edge in enumerate(sorted(spectrum_usage.keys()))}
+
+        for request, alloc in spectrum_allocation.items():
+            start_col = alloc['fsu_index']
+            num_cols = alloc['fsu_needed']
+            path = alloc['path']
+            for node in range(len(path) - 1):
+                edge = (path[node], path[node + 1])
+                row = edge_to_row[edge]
+                ax.add_patch(Rectangle((start_col, row), num_cols, 1, color=color_map[request]))
+                ax.text(start_col + num_cols / 2, row + 0.5, str(request), color='white', ha='center', va='center')
+
+        for i, request in enumerate(spectrum_allocation_backup.keys()):
+            color_map[request] = plt.cm.tab20(i % 20)
+
+        edge_to_row = {edge: i for i, edge in enumerate(sorted(spectrum_usage.keys()))}
+
+        for request, alloc in spectrum_allocation_backup.items():
+            start_col = alloc['fsu_index']
+            num_cols = alloc['fsu_needed']
+            path = alloc['path']
+            for node in range(len(path) - 1):
+                edge = (path[node], path[node + 1])
+                row = edge_to_row[edge]
+                ax.add_patch(Rectangle((start_col, row), num_cols, 1, fill=False, color=color_map[request],hatch='//' ))
+                ax.text(start_col + num_cols / 2, row + 0.5, str(request), color='black', ha='center', va='center')
+
+
+        ax.set_xlim(0, FSU_CAPACITY)
+        ax.set_ylim(0, len(spectrum_usage))
+        ax.set_yticks(list(range(1, len(spectrum_usage) + 1)))
+        ax.set_yticklabels([f"{edge[0]}:{edge[1]}" for edge in spectrum_usage.keys()])
+        ax.set_xlabel('FSU Index')
+        ax.set_ylabel('Edge')
+        ax.set_title('Spectrum Allocation Grid')
+        plt.savefig('objective_data_fig/spectrum_allocation-shared-' + traffic_file_name + '.png')
+        plt.show()
+
+    draw_spectrum_allocation(best_state, best_state_backup, spectrum_allocation, spectrum_allocation_backup)
+
+    with open('objective_data_fig/spectrum_allocation-shared-'+traffic_file_name+'.pkl', 'wb') as f:
+        pickle.dump(spectrum_allocation, f)
+        pickle.dump(edge_traffic_details, f)
+        pickle.dump(spectrum_usage, f)
+
+
+    def performance_calculation(spectrum_usage_shared,best_state,G,COST_PER_CABLE,FSU_PER_CABLE):
+        num_fsu_used = 0
+        highest_load=0
+        highest_fsu=0
+        total_length=0
+        total_cost=0
+        for request in spectrum_usage_shared:
+            num_fsu_used+=sum(spectrum_usage_shared[request])
+            if sum(spectrum_usage_shared[request])>highest_load:
+                highest_load=sum(spectrum_usage_shared[request])
+            try:
+                if (len(spectrum_usage_shared[request]) - spectrum_usage_shared[request][::-1].index(1))>highest_fsu:
+                    highest_fsu=(len(spectrum_usage_shared[request]) - spectrum_usage_shared[request][::-1].index(1))
+            except:
+                pass
+        average_fsu_used=math.ceil(num_fsu_used/len(spectrum_usage_shared))
+        for request in best_state:
+            for i in range(len(best_state[request])-1):
+                total_length+=G[best_state[request][i]][best_state[request][i+1]]['weight']
+
+        average_path_length=math.ceil(total_length/len(best_state))
+        total_cost=(num_fsu_used/FSU_PER_CABLE)*COST_PER_CABLE
+        return num_fsu_used,highest_load,highest_fsu,average_fsu_used,average_path_length,total_cost
+    for request in spectrum_usage_shared:
+        for i in range(len(spectrum_usage_shared[request])-1):
+            if spectrum_usage_shared[request][i]!=0:
+                spectrum_usage_shared[request][i]=1
+    print(performance_calculation(spectrum_usage_shared,best_state,G,COST_PER_CABLE,FSU_PER_CABLE))
+    return performance_calculation(spectrum_usage_shared,best_state,G,COST_PER_CABLE,FSU_PER_CABLE)
+
+if __name__ == '__main__':
+    num_fsu_used,highest_load,highest_fsu,average_fsu_used,average_path_length,total_cost=shared_protection('G7-topology.txt', 'G7-matrix-5.txt')
+    # print("Done")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
